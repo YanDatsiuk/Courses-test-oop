6ebf0844525effe3ec149c0632a8f3d25c974c53
datsyuk <yan.datsiuk@thinkmobiles.com>
2017-11-02 13:44:37 +0200

    added validation exceptions for duration and price

--- src/Concerns/IsLesson.php
+++ src/Concerns/IsLesson.php
@@ -3,34 +3,45 @@
 namespace YanDatsyuk\Courses\Concerns;
 
 use YanDatsyuk\Courses\Core\RateType;
+use YanDatsyuk\Courses\Exceptions\IncorrectPriceValueException;
 use YanDatsyuk\Courses\Exceptions\UnknownRateTypeException;
 
 /**
- * Created by PhpStorm.
- * User: datsyuk
- * Date: 02.11.17
- * Time: 13:05
+ * Trait IsLesson
+ * @package YanDatsyuk\Courses\Concerns
  */
 trait IsLesson
 {
-    //lesson duration
+    /**
+     * Duration for a lesson.
+     *
+     * @var $duration float
+     */
     private $duration;
 
-    //type of this lesson
-    private $rateType;
-
-    //
+    /**
+     * Price value for a fixed rate or for an hour.
+     *
+     * @var $price float
+     */
     private $price;
+
+    /**
+     * Rate type for a lesson.
+     *
+     * @var $rateType string
+     */
+    private $rateType;
 
 
     /**
-     * Calculate price for a lesson.
+     * Calculate total price for a lesson.
      * Price depends on rate type of this lesson.
      *
      * @return float
      * @throws UnknownRateTypeException
      */
-    public function getPrice(): float
+    public function getTotalPrice(): float
     {
         switch ($this->rateType) {
             case RateType::FIXED:
@@ -54,5 +65,64 @@ trait IsLesson
     private function _calculatePriceForHourlyRate(): float
     {
         return $this->price * $this->duration;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrice():float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param $price
+     * @throws IncorrectPriceValueException
+     */
+    public function setPrice($price)
+    {
+        //price validation
+        if ($price < 0){
+            throw new IncorrectPriceValueException();
+        }
+        $this->price = $price;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuration():float
+    {
+        return $this->duration;
+    }
+
+    /**
+     * @param $duration
+     * @throws IncorrectPriceValueException
+     */
+    public function setDuration($duration)
+    {
+        //duration validation
+        if ($duration < 0){
+            throw new IncorrectPriceValueException();
+        }
+
+        $this->duration = $duration;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRateType(): string
+    {
+        return $this->rateType;
+    }
+
+    /**
+     * @param string $rateType
+     */
+    public function setRateType(string $rateType)
+    {
+        $this->rateType = $rateType;
     }
 }
---
+++ src/Exceptions/IncorrectPriceValueException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace YanDatsyuk\Courses\Exceptions;
+
+use Throwable;
+
+/**
+ * Created by PhpStorm.
+ * User: datsyuk
+ * Date: 02.11.17
+ * Time: 13:24
+ */
+
+class IncorrectPriceValueException extends \Exception
+{
+    public function __construct($message = "IncorrectPriceValueException", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
